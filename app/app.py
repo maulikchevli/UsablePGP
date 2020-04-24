668f73b1ce9661364f06828cfb08f1350af9c987
maulikchevli <maulikchevli@gmail.com>
2020-04-25 00:10:39 +0530

    Add dummy Register route

--- app/app.py
+++ app/app.py
@@ -1,13 +1,51 @@
 # flask related imports
-from flask import Flask
+from flask import Flask, request, jsonify
 
 # other python libs
 import sys
+import requests
+
+from stub import *
 
 app = Flask(__name__)
-@app.route('/')
+
+API_ROUTE = {
+    "test_api" : "http://localhost:5000/test_api/"
+}
+
+@app.route('/', methods = ['GET'])
 def index():
     return "User Inerface controller"
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return "Register.html"
+    else:
+        user_id = get_form_field('user_id')
+        pwd = get_form_field('pwd')
+
+        """
+        Generate keys
+        """
+        success = generate_keys(user_id, pwd)
+        if not success:
+            # Panic and exit
+            sys.exit(-1)
+
+        """
+        Call api to store keys
+        """
+        server_resp = requests.post(
+            API_ROUTE['test_api'],
+            json = {'user_id': user_id}
+        )
+
+        if server_resp.json()['status'] == "success":
+            return "Render Home screen"
+        else:
+            # Try again?
+            return "Could not add to DB"
 
 if __name__ == "__main__":
     host = sys.argv[1]
---
+++ app/stub.py
@@ -0,0 +1,6 @@
+def generate_keys(user_id, pwd):
+    return True
+
+def get_form_field(field_name):
+    # Return static for now
+    return "u16co"
