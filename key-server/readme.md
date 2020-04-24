18cb3a23e8996f1ad3da5c6fb964033bd94232d2
maulikchevli <maulikchevli@gmail.com>
2020-04-24 14:44:12 +0530

    Add key-server boiler plate

---
+++ key-server/readme.md
@@ -0,0 +1,3 @@
+### Usage
+python server.py _ip-addr_ _port_
+
---
+++ key-server/server.py
@@ -0,0 +1,20 @@
+# flask related imports
+from flask import Flask
+
+# other python libs
+import sys
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Key Server"
+
+if __name__ == "__main__":
+    host = sys.argv[1]
+    port = sys.argv[2]
+
+    app.run(
+        host = host,
+        port = port,
+        debug = True
+    )
