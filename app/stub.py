3af48bcc1b5e40bb2e7479f59d0a58f8ceba74aa
maulikchevli <maulikchevli@gmail.com>
2020-04-25 18:00:21 +0530

    Integrate Register with key server

--- app/stub.py
+++ app/stub.py
@@ -1,6 +1,12 @@
+from flask import request
+
 def generate_keys(user_id, pwd):
-    return True
+    return "test pu key", 1111, True
 
 def get_form_field(field_name):
-    # Return static for now
-    return "u16co"
+    if not request.form:
+        # Return static for now
+        return "u16co"
+    else:
+        return request.form[field_name]
+
