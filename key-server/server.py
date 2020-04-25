7296a0b8179b21df671931887f7c8472dee9e558
Suprit Bhattacharjee <suprit98@gmail.com>
2020-04-25 12:12:01 +0530

    POST API done

--- key-server/server.py
+++ key-server/server.py
@@ -1,10 +1,19 @@
 # flask related imports
 from flask import Flask
 from flask import jsonify
-from flask import request
+from flask import request,abort
+
 
 # other python libs
 import sys
+import sqlite3 as sql
+
+
+def dict_factory(cursor,row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 app = Flask(__name__)
 @app.route('/')
@@ -15,6 +24,36 @@ def index():
 def test():
     tmp = request.get_json()
     return jsonify({'status': 'success'})
+
+@app.route('/insert_users/',methods=['POST'])
+def insert_users():
+    if not request.json or not 'username' in request.json:
+        print('aborted')
+        abort(400)
+
+    username = request.json['username']
+    public_key = request.json['public_key']
+    salt = request.json['salt']
+
+    status = "failure"
+    with sql.connect("database.db") as con:
+        con.row_factory = dict_factory
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM users WHERE username=?",(str(username),))
+        users = cur.fetchall()
+
+        if users:
+            status = "failure"
+        else:
+            print("inserting")
+            cur.execute("INSERT INTO users (username,public_key,salt) VALUES (?,?,?)",(str(username),str(public_key),str(salt)))
+            print("inserted")
+            con.commit()
+            print("comitted")
+            status = "success"
+    return jsonify({'status':status});
+
 
 if __name__ == "__main__":
     host = sys.argv[1]
