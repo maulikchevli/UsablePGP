09426a2f8c05e05323883f2385845973cc0d8b2f
rrathi0705 <rrathi0705@gmail.com>
2020-04-26 17:05:53 +0530

    Enc_sign.html modified

--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 	$("#fileBox").hide()
 	$("#messageBox").hide()
+	$("#searchBox").hide()
+	$("#submitButton").hide()
 	$("#messageformatfile").click(function(){
 		$("#fileBox").show()
-		$("#messageBox").hide()	
+		$("#messageBox").hide()
+		$("#searchBox").show()
+		$("#submitButton").show()	
 	});
 
 	$("#messageformattext").click(function(){
 		$("#messageBox").show()
 		$("#fileBox").hide()
+		$("#searchBox").show()
+		$("#submitButton").show()
 	});
 });
