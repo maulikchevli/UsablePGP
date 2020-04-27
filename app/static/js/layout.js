7379ee36c8bbe96a3547adff45be68279bcd50ae
rrathi0705 <rrathi0705@gmail.com>
2020-04-27 14:56:24 +0530

    Passphrase added in enc_sig and javascript validation added

--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -8,37 +8,52 @@ $(document).ready(function(){
 		$("#fileBox").show()
 		$("#messageBox").hide()
 		$("#submitButton").show()
+		$("#message").val("")
 	});
 	$("#messageformattext").click(function(){
 		$("#messageBox").show()
 		$("#fileBox").hide()
 		$("#submitButton").show()
+		$("#file").val(null)
 	});
 
 	$("#formSubmit").click(function(){
 		var checked = $('input:checked').length;
+		alert($("input[name='username']").val())
 		if (checked<2){
 	        alert("Please check at least one of Encyption or Sign");
 	        return false;
 	    }
+	    if($("#messageBox").is(":visible") && $("textarea#message").val() == ""){
+	    	alert("Please Enter the Message");
+	    	return false;
+	    }
+	    if($("#fileBox").is(":visible") && $("input[name='file']").val() == ""){
+	    	alert("Please Select the File");
+	    	return false;
+	    }
+	    if($("#passphraseBox").is(":visible") && $("input[name='passphrase']").val() == ""){
+	    	alert("Please Enter the Passphrase");
+	    	return false;
+	    }
+	    if($("#searchBox").is(":visible") && $("input[name='username']").val() == ""){
+	    	alert("Please Enter the Username");
+	    	return false;
+	    }
+	    
 	});
-	$("#encrypt").click(function(){
+	$("#encrypt, #verify").click(function(){
 		if($("#searchBox").is(":visible")){
 			$("#searchBox").hide()
+			$("input[name='username']").val("")
 		}else{
 			$("#searchBox").show()
 		}
 	});
-	$("#verify").click(function(){
-		if($("#searchBox").is(":visible")){
-			$("#searchBox").hide()
-		}else{
-			$("#searchBox").show()
-		}
-	});
-	$("#decrypt").click(function(){
+	$("#sign, #decrypt").click(function(){
 		if($("#passphraseBox").is(":visible")){
 			$("#passphraseBox").hide()
+			$("input[name='passphrase']").val("")
 		}else{
 			$("#passphraseBox").show()
 		}
@@ -48,6 +63,22 @@ $(document).ready(function(){
 		if (checked<2){
 	        alert("Please check at least one of Decryption or Verify");
 	        return false;
+	    }
+	    if($("#messageBox").is(":visible") && $("textarea#message").val() == ""){
+	    	alert("Please Enter the Message");
+	    	return false;
+	    }
+	    if($("#fileBox").is(":visible") && $("input[name='file']").val() == ""){
+	    	alert("Please Select the File");
+	    	return false;
+	    }
+	    if($("#passphraseBox").is(":visible") && $("input[name='passphrase']").val() == ""){
+	    	alert("Please Enter the Passphrase");
+	    	return false;
+	    }
+	    if($("#searchBox").is(":visible") && $("input[name='username']").val() == ""){
+	    	alert("Please Enter the Username");
+	    	return false;
 	    }
 	});
 });
