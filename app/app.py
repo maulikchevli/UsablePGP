cce6a4e8207f4bc1a3ecaf4181d77860697cbdd7
rrathi0705 <rrathi0705@gmail.com>
2020-04-28 17:33:53 +0530

    Integrated KeyProperty.html with jinja

--- app/app.py
+++ app/app.py
@@ -340,7 +340,7 @@ def revoke_regen():
 
 @app.route('/key_prop', methods=['GET'])
 def key_prop():
-    return render_template('key_property.html')
+    return render_template('KeyProperty.html')
 
 if __name__ == "__main__":
     host = sys.argv[1]
